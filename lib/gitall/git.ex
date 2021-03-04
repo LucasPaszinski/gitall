8ca260fa5560439273f3f70214bc1180f7338c42
lucaspaszinski <lucaspaszinski@gmail.com>
2021-03-04 18:16:34 -0300

    Git execute command

---
+++ lib/gitall/git.ex
@@ -0,0 +1,22 @@
+defmodule Gitall.Git do
+  def execute(path, command)
+      when is_binary(path) and is_binary(command) do
+    with ["git" = git | params] <- sanitize_command(command) do
+      execute_command(git, params, path)
+    else
+      MatchError -> {:error, "is not a git command"}
+    end
+  end
+
+  def sanitize_command(command) do
+    command
+    |> String.split()
+  end
+
+  defp execute_command(root_cmd, params, path) do
+    case System.cmd(root_cmd, params, cd: path, stderr_to_stdout: true) do
+      {msg, 0} -> {:ok, msg}
+      {msg, _} -> {:error, msg}
+    end
+  end
+end
